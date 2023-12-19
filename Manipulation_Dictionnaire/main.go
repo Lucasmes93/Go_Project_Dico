468d4a8efaa7d77656b61f5a62f992c6e3f72bae
Lucas MESSIA <lmessia93@gmail.com>
2023-12-19 12:53:14 +0100

    Update main.go

--- Manipulation_Dictionnaire/main.go
+++ Manipulation_Dictionnaire/main.go
@@ -42,7 +42,7 @@ func main() {
 	// Ajoutez des mots et des définitions
 	d.Add("chat", "Un animal domestique à quatre pattes.")
 	d.Add("chien", "Un autre animal domestique à quatre pattes.")
-	d.Add("oiseau", "Un animal volant ayant deux pattes.")
+	d.Add("oiseau", "Un animal volant.")
 
 	// Obtenez la définition d'un mot
 	definition, ok := d.Get("chat")
