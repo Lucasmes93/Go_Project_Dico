aeca5766c4f1dca18fcdb313183741f0f48b5b2d
Lucas MESSIA <lmessia93@gmail.com>
2023-12-19 15:14:11 +0100

    Update main.go

--- Manipulation_Dictionnaire/main.go
+++ Manipulation_Dictionnaire/main.go
@@ -5,30 +5,28 @@ import (
 	"sort"
 )
 
-type Dictionary struct {
-	m map[string]string
-}
+type Dictionary map[string]string
 
 func NewDictionary() Dictionary {
-	return Dictionary{m: make(map[string]string)}
+	return make(Dictionary)
 }
 
-func (d *Dictionary) Add(word, definition string) {
-	d.m[word] = definition
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
 }
 
-func (d *Dictionary) Get(word string) (string, bool) {
-	definition, ok := d.m[word]
+func (d Dictionary) Get(word string) (string, bool) {
+	definition, ok := d[word]
 	return definition, ok
 }
 
-func (d *Dictionary) Remove(word string) {
-	delete(d.m, word)
+func (d Dictionary) Remove(word string) {
+	delete(d, word)
 }
 
-func (d *Dictionary) List() []string {
+func (d Dictionary) List() []string {
 	var words []string
-	for word := range d.m {
+	for word := range d {
 		words = append(words, word)
 	}
 	sort.Strings(words)
@@ -36,24 +34,19 @@ func (d *Dictionary) List() []string {
 }
 
 func main() {
-	// Créez un nouveau dictionnaire
 	d := NewDictionary()
 
-	// Ajoutez des mots et des définitions
 	d.Add("chat", "Mammifère carnivore (félidé), sauvage ou domestique, au museau court et arrondi.")
 	d.Add("chien", "Mammifère (canidé) carnivore aux multiples races, caractérisé par sa facilité à être domestiqué, par une course rapide, un excellent odorat et par son cri spécifique, l'aboiement.")
 	d.Add("oiseau", "Vertébré ovipare, couvert de plumes et d'écailles cornées, à respiration pulmonaire, homéotherme, aux mâchoires sans dents revêtues d'un bec corné, et aux membres antérieurs, ou ailes, normalement adaptés au vol.")
 
-	// Obtenez la définition d'un mot
 	definition, ok := d.Get("chat")
 	if ok {
 		fmt.Println("Définition de 'chat':", definition)
 	}
 
-	// Supprimez un mot
 	d.Remove("oiseau")
 
-	// Obtenez la liste triée des mots
 	words := d.List()
 	for _, word := range words {
 		fmt.Println(word)
