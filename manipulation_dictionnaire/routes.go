9f7e2aa0e5f81f4f7487836a4770c085804b7d45
Lucas MESSIA <lmessia93@gmail.com>
2023-12-20 16:45:11 +0100

    Create routes.go
    Ajout de la configuration des routes HTTP pour le dictionnaire.
    La fonction SetupRoutes a été introduite dans le package manipulation_dictionnaire
    pour configurer les routes HTTP permettant l'ajout, la récupération, la suppression et l'énumération d'éléments dans le dictionnaire. Cela facilitera l'intégration de ces fonctionnalités dans une application web en utilisant le package net/http.

---
+++ manipulation_dictionnaire/routes.go
@@ -0,0 +1,11 @@
+package manipulation_dictionnaire
+
+import "net/http"
+
+// SetupRoutes configure les routes pour le dictionnaire
+func SetupRoutes(dictionary *Dictionary) {
+	http.HandleFunc("/add", dictionary.Add)
+	http.HandleFunc("/get", dictionary.Get)
+	http.HandleFunc("/remove", dictionary.Remove)
+	http.HandleFunc("/list", dictionary.List)
+}
