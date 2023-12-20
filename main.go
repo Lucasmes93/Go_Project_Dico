1d6b7584d4ac8438110b67d33b0d49e72998cb85
Lucas MESSIA <lmessia93@gmail.com>
2023-12-20 10:11:36 +0100

    Update main Get

--- main.go
+++ main.go
@@ -12,10 +12,12 @@ func main() {
 	// Ajout de mots et de définitions
 	handleError(d.Add("chat", "Mammifère carnivore (félidé), sauvage ou domestique, au museau court et arrondi."), "Erreur lors de l'ajout")
 
-	// Récupère et affiche la définition du mot "chat"
+	// Récupération de la définition du mot "chat"
 	definition, ok := d.Get("chat")
 	if ok {
 		fmt.Println("Définition de 'chat':", definition)
+	} else {
+		fmt.Println("Mot non trouvé.")
 	}
 
 	// Supprime le mot "oiseau" du dictionnaire
