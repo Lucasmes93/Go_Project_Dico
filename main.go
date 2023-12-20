a07cb710315cd461b5d55c901df03a41dd701cd5
Lucas MESSIA <lmessia93@gmail.com>
2023-12-20 12:31:44 +0100

    Update main add
    Gestion Concurrente avec Channels en Go

--- main.go
+++ main.go
@@ -10,9 +10,12 @@ func main() {
 	d := manipulation_dictionnaire.NewDictionary(filePath)
 
 	// Ajout de mots et de définitions
-	handleError(d.Add("chat", "Mammifère carnivore (félidé), sauvage ou domestique, au museau court et arrondi."), "Erreur lors de l'ajout")
-	handleError(d.Add("chien", "Mammifère (canidé) carnivore aux multiples races, caractérisé par sa facilité à être domestiqué, par une course rapide, un excellent odorat et par son cri spécifique, l'aboiement."), "Erreur lors de l'ajout")
-	handleError(d.Add("oiseau", "Vertébré ovipare, couvert de plumes et d'écailles cornées, à respiration pulmonaire, homéotherme, aux mâchoires sans dents revêtues d'un bec corné, et aux membres antérieurs, ou ailes, normalement adaptés au vol."), "Erreur lors de l'ajout")
+	go func() {
+		handleError(d.Add("chat", "Mammifère carnivore (félidé), sauvage ou domestique, au museau court et arrondi."), "Erreur lors de l'ajout")
+		handleError(d.Add("chien", "Mammifère (canidé) carnivore aux multiples races, caractérisé par sa facilité à être domestiqué, par une course rapide, un excellent odorat et par son cri spécifique, l'aboiement."), "Erreur lors de l'ajout")
+		handleError(d.Add("oiseau", "Vertébré ovipare, couvert de plumes et d'écailles cornées, à respiration pulmonaire, homéotherme, aux mâchoires sans dents revêtues d'un bec corné, et aux membres antérieurs, ou ailes, normalement adaptés au vol."), "Erreur lors de l'ajout")
+
+	}()
 
 	// Récupération de la définition du mot "chat"
 	definition, ok := d.Get("chat")
